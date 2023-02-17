57a5ba60fa4df062fa9baab38fefa1fb1481754b
socheata_thai <socheatathai62@gmail.com>
2023-02-17 13:55:54 +0700

    db config

--- app/config/mysql.config.js
+++ app/config/mysql.config.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql");
+// const database = mysql.createConnection({
+//     host : "localhost",
+//     database : "node_api",
+//     user : "root",
+//     password :""
+// })
 const database = mysql.createConnection({
-    host : "localhost",
-    database : "node_api",
-    user : "root",
-    password :""
+    host : "bqltdxftggqbrnaij7zt-mysql.services.clever-cloud.com",
+    database : "bqltdxftggqbrnaij7zt",
+    user : "ub2lhdhtlbaixyxv",
+    password :"QvY1gCUB6FjBfM5LGKtf"
 })
 database.connect();
 module.exports = database;
